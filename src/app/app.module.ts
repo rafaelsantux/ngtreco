b6464102e3439b8aaf007dc235a33947cdd3eac0
rafaelsantux <rafaelsantux0411@gmail.com>
2023-06-28 21:42:21 -0300

    Update
    Falta alterar o html de home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,6 +23,7 @@ import { ThemeComponent } from './theme/theme.component';
     ThemeComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  articles: any;
+
+  constructor(
+    private http: HttpClient
+  ){}
+
+  ngOnInit() {
+    this.getAllArticles();
+  }
+
+  getAllArticles() {
+
+    this.http.get('http://localhost:8080/articles')
+    .subscribe((response) =>{
+      console.log(response);
+      this.articles = response;
+    },
+      (error) => {
+        console.log(error);
+      }
+
+
+    );
+  }
 
 }
